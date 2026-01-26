4d02d4ace0bbb8b1976f0e7d1c900931147986c2
Ufurus <slavkata72@gmail.com>
2026-01-26 23:30:41 +0200

    more error handling tasks

---
+++ Error handling/rotate_matrix.py
@@ -0,0 +1,40 @@
+class MatrixContentError(Exception):
+    pass
+
+class MatrixSizeError(Exception):
+    pass
+
+def rotate_matrix(matrix):
+    matrix_length = len(matrix)
+
+    for row in matrix:
+        if len(row) != matrix_length:
+            raise MatrixSizeError("The size of the matrix is not a perfect square")
+
+    for row in matrix:
+        for col in row:
+            if col.isdigit():
+                pass
+            else:
+                raise MatrixContentError("The matrix must consist of only integers")
+
+    for i in range(matrix_length):
+        for j in range(i, matrix_length):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(matrix_length):
+        matrix[i].reverse()
+
+mtrx = []
+
+while True:
+    line = input().split()
+
+    if not line:
+        break
+    mtrx.append(line)
+
+rotate_matrix(mtrx)
+
+for row in mtrx:
+    print(*row, sep=' ')

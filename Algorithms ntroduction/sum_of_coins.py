f72e3c60d57464c8f0a591d49683498310997537
Ufurus <slavkata72@gmail.com>
2026-02-19 02:23:55 +0200

    Create sum_of_coins.py

---
+++ Algorithms ntroduction/sum_of_coins.py
@@ -0,0 +1,26 @@
+def calc_coins(coins, target):
+    coins.sort(reverse=True)
+    used_coins = {}
+
+    index = 0
+    while target != 0 and index < len(coins):
+        count_coins = target // coins[index]
+        target %= coins[index]
+
+        if count_coins > 0:
+            used_coins[coins[index]] = count_coins
+
+        index += 1
+
+    if target != 0:
+        return "Error"
+
+    result = f"Number of coins to take: {sum(used_coins.values())}\n"
+    for value, count in used_coins.items():
+        result += f"{count} coin(s) with value {value}\n"
+
+    return result
+
+coins_list = [int(x) for x in input().split(", ")]
+target_number = int(input())
+print(calc_coins(coins_list, target_number))

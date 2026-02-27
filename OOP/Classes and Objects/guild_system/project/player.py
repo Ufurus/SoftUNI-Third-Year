5dc4666e5810882073133ad4e1bf5a6f08598680
Ufurus <slavkata72@gmail.com>
2026-02-27 04:07:55 +0200

    Create player.py

---
+++ OOP/Classes and Objects/guild_system/project/player.py
@@ -0,0 +1,21 @@
+class Player:
+    NO_GUILD = 'Unaffiliated'
+
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills: dict = {}
+        self.guild: str = Player.NO_GUILD
+
+    def add_skill(self, skill_name, mana_cost) -> str:
+        if skill_name in self.skills:
+            return f"Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        total_info = [f"Name: {self.name}", f"Guild: {self.guild}", f"HP: {self.hp}", f"MP: {self.mp}"]
+        for skill, mana in self.skills.items():
+            total_info.append(f"==={skill} - {mana}")
+        return "\n".join(total_info)

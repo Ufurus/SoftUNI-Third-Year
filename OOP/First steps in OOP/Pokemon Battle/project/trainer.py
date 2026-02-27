d61d74d0d894d3336487090466b8c48aa3658abb
Ufurus <slavkata72@gmail.com>
2026-02-27 04:08:02 +0200

    Update trainer.py

--- OOP/First steps in OOP/Pokemon Battle/project/trainer.py
+++ OOP/First steps in OOP/Pokemon Battle/project/trainer.py
@@ -5,22 +5,24 @@ class Trainer:
         self.name = name
         self.pokemons: list[Pokemon] = []
     def add_pokemon(self, pokemon: Pokemon):
-        if pokemon in self.pokemons:
+        poke_details = pokemon.pokemon_details()
+        if poke_details not in self.pokemons:
+            self.pokemons.append(poke_details)
+            return f"Caught {poke_details}"
+        else:
             return f"This pokemon is already caught"
-        self.pokemons.append(pokemon)
-        return f"Caught {pokemon.pokemon_details()}"
 
     def release_pokemon(self, pokemon_name: str):
-        pokemon_to_release = next((p for p in self.pokemons if p.name == pokemon_name), None)
-        if pokemon_to_release:
-            self.pokemons.remove(pokemon_to_release)
-            return f"You have released {pokemon_name}"
-        return "Pokemon is not caught"
-
-    def trainer_data(self) -> str:
-        final_result = [f"Pokemon trainer {self.name}",
-                        f"Pokemon count {len(self.pokemons)}"]
         for poke in self.pokemons:
-            final_result.append(f"- {poke.pokemon_details()}")
+            if poke.split()[0] == pokemon_name:
+                self.pokemons.remove(poke)
+                return f"You have released {pokemon_name}"
+        else:
+            return f"Pokemon is not caught"
 
-        return '\n'.join(final_result)
+    def trainer_data(self):
+        final_result = f"Pokemon Trainer {self.name}\n"
+        final_result += f"Pokemon count {len(self.pokemons)}\n"
+        for poke in self.pokemons:
+            final_result += f"- {poke}\n"
+        return final_result.strip()

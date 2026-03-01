b18b86ba35a17cd8c2c45126b1faf9dfb9d5a649
Ufurus <slavkata72@gmail.com>
2026-03-01 12:29:53 +0200

    More inheritance tasks

---
+++ OOP/Inheritance/Exercises/Zoo/main.py
@@ -0,0 +1,13 @@
+from project.animal import Animal
+from project.mammal import Mammal
+from project.reptile import Reptile
+from project.lizard import Lizard
+from project.gorilla import Gorilla
+from project.snake import Snake
+
+mammal = Mammal("Stella")
+print(mammal.__class__.__bases__[0].__name__)
+print(mammal.name)
+lizard = Lizard("John")
+print(lizard.__class__.__bases__[0].__name__)
+print(lizard.name)

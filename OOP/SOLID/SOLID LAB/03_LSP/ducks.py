0a6fd52ccf06323a95a27bab2de53a5d829af7db
Ufurus <slavkata72@gmail.com>
2026-03-19 12:21:52 +0200

    More SOLID principle tasks

---
+++ OOP/SOLID/SOLID LAB/03_LSP/ducks.py
@@ -0,0 +1,53 @@
+from abc import abstractmethod, ABC
+
+class Duck(ABC):
+    @staticmethod
+    def quack():
+        pass
+
+    @staticmethod
+    def walk():
+        pass
+
+    @staticmethod
+    def fly():
+        pass
+
+class BaseToy(ABC):
+    @staticmethod
+    def make_sound():
+        pass
+
+class RubberDuck(BaseToy):
+    @staticmethod
+    def make_sound():
+        return "Squeek"
+
+class RobotDuck(Duck):
+    HEIGHT = 50
+
+    def __init__(self):
+        self.height = 0
+
+    @staticmethod
+    def quack():
+        return 'Robotic quacking'
+
+    @staticmethod
+    def walk():
+        return 'Robotic walking'
+
+    def fly(self):
+        """can only fly to specific height but
+        when it reaches it starts landing automatically"""
+        if self.height == RobotDuck.HEIGHT:
+            self.land()
+        else:
+            self.height += 1
+
+    def land(self):
+        self.height = 0
+
+
+
+

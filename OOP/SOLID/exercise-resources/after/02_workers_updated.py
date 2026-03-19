b2361dfe374887975a88d0f4953f4d6d526731f3
Ufurus <slavkata72@gmail.com>
2026-03-19 14:10:50 +0200

    More SOLID exercises

--- OOP/SOLID/exercise-resources/after/02_workers_updated.py
+++ OOP/SOLID/exercise-resources/after/02_workers_updated.py
@@ -1,18 +1,18 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 import time
 
-class AbstractWorker:
-    __metaclass__ = ABCMeta
+class Workable(ABC):
 
     @abstractmethod
     def work(self):
         pass
 
+class Eatable(ABC):
     @abstractmethod
     def eat(self):
         pass
 
-class Worker(AbstractWorker):
+class Worker(Workable, Eatable):
 
     def work(self):
         print("I'm normal worker. I'm working.")
@@ -21,7 +21,7 @@ class Worker(AbstractWorker):
         print("Lunch break....(5 secs)")
         time.sleep(5)
 
-class SuperWorker(AbstractWorker):
+class SuperWorker(Worker, Eatable):
 
     def work(self):
         print("I'm super worker. I work very hard!")
@@ -30,41 +30,57 @@ class SuperWorker(AbstractWorker):
         print("Lunch break....(3 secs)")
         time.sleep(3)
 
-
-class Manager:
+class BaseManager(ABC):
 
     def __init__(self):
         self.worker = None
 
+    @abstractmethod
     def set_worker(self, worker):
-        assert isinstance(worker, AbstractWorker), "`worker` must be of type {}".format(AbstractWorker)
+        pass
+
+class WorkManager(BaseManager):
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Workable), "`worker` must be of type {}".format(Workable)
 
         self.worker = worker
 
     def manage(self):
         self.worker.work()
 
+class BreakManager(BaseManager):
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Eatable), "`worker` must be of type {}".format(Eatable)
+
+        self.worker = worker
+
     def lunch_break(self):
         self.worker.eat()
 
-class Robot(AbstractWorker):
+class Robot(Workable):
 
     def work(self):
         print("I'm a robot. I'm working....")
 
-    def eat(self):
-        print("I don't need to eat....")
+work_manager = WorkManager()
+break_manager = BreakManager()
+work_manager.set_worker(Worker())
+break_manager.set_worker(Worker())
+work_manager.manage()
+break_manager.lunch_break()
 
+work_manager.set_worker(SuperWorker())
+break_manager.set_worker(SuperWorker())
+work_manager.manage()
+break_manager.lunch_break()
 
-manager = Manager()
-manager.set_worker(Worker())
-manager.manage()
-manager.lunch_break()
+work_manager.set_worker(Robot())
+work_manager.manage()
+try:
+    break_manager.set_worker(Robot())
+    break_manager.lunch_break()
+except:
+    pass
 
-manager.set_worker(SuperWorker())
-manager.manage()
-manager.lunch_break()
-
-manager.set_worker(Robot())
-manager.manage()
-manager.lunch_break()
